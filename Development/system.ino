a63968e526f0957a0c42df8a16c9ab2f27b348d1
Bella Conrad <isabella-conrad@uiowa.edu>
2025-10-03 00:54:56 -0500

    add wifi loop commented out

--- Development/system.ino
+++ Development/system.ino
@@ -47,7 +47,7 @@ void setup() {
     pinMode(BUTTON1, INPUT_PULLUP);
     pinMode(BUTTON2, INPUT_PULLUP);
     // On/off switch (use INPUT_PULLUP or INPUT depending on wiring)
-    pinMode(ONOFFSWITCH, INPUT)
+    pinMode(ONOFFSWITCH, INPUT);
 
     // Initialize Serial Monitor
     Serial.begin(115200);
@@ -94,15 +94,10 @@ void setup() {
 
 // =======================  Main loop =======================
 void loop() {
-    WifiClient client = server.available();
-    if (client) {
-        Serial.println("New Client.");
-    }
+  WiFiClient client = server.available();   // Check for incoming client
 
-
-  // Check on/off switch
+  // Check ON/OFF switch
   bool switchState = digitalRead(ONOFFSWITCH);
-
   if (switchState == HIGH) {
     // Show "No data available"
     lcd.clear();
@@ -110,34 +105,42 @@ void loop() {
     lcd.print("No data");
     lcd.setCursor(0, 1);
     lcd.print("available");
-    delay(200);  // Small delay to avoid flicker
-    lcd.clear();
-    return;      // Skip the rest of loop
+    delay(200);
+
+    // Disable server response
+    if (client) {
+      client.stop(); 
+      Serial.println("Client disconnected.");
+    }
+    return;     
   }
 
-  // Read button states (LOW = pressed, HIGH = released)
+  // ----- Normal hardware logic (buttons + LCD) -----
   bool currentButton1State = digitalRead(BUTTON1);
   bool currentButton2State = digitalRead(BUTTON2);
-  // Check for button 1 press (falling edge)
+
   if (currentButton1State == LOW && lastButton1State == HIGH) {
     lastSensor1State = sensor1State;
-    sensor1State = !sensor1State;  // Toggle state
-    //delay(50);  // Simple debounce
+    sensor1State = !sensor1State;
   }
-  // Check for button 2 press (falling edge)
   if (currentButton2State == LOW && lastButton2State == HIGH) {
     lastSensor2State = sensor2State;
-    sensor2State = !sensor2State;  // Toggle state
-    //delay(50);  // Simple debounce
+    sensor2State = !sensor2State;
   }
-  // Update last button states
+
   lastButton1State = currentButton1State;
   lastButton2State = currentButton2State;
 
+  // Read sensors
+  sensors.requestTemperatures();
+  float tempC1 = sensors.getTempCByIndex(0);
+  float tempC2 = sensors.getTempCByIndex(1);
 
-  // Update temperature display
-  sensors.requestTemperatures(); // MAYBE MOVE THIS TO INSIDE IF STATEMENTS?
+  // Save temps for webpage response
+  tempsC[0] = tempC1; tempsF[0] = DallasTemperature::toFahrenheit(tempC1);
+  tempsC[1] = tempC2; tempsF[1] = DallasTemperature::toFahrenheit(tempC2);
 
+  // LCD update logic ===============================
   // Always update temperature if sensor 1 is ON
   if (sensor1State) {
       // Sensor 1 display
@@ -147,7 +150,7 @@ void loop() {
       lcd.setCursor(0, 1);
       lcd.print("S2:");
     lcd.setCursor(4, 0); // Position for Sensor 1 reading
-    float tempC1 = sensors.getTempCByIndex(0);
+    tempC1 = sensors.getTempCByIndex(0);
     if (tempC1 == DEVICE_DISCONNECTED_C) {
       lcd.print("Error     ");
     } else {
@@ -176,7 +179,7 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print("S2:");
     lcd.setCursor(4, 1); // Position for Sensor 2 reading
-    float tempC2 = sensors.getTempCByIndex(1);
+    tempC2 = sensors.getTempCByIndex(1);
     if (tempC2 == DEVICE_DISCONNECTED_C) {
       lcd.print("Error     ");
     } else {
@@ -196,5 +199,68 @@ void loop() {
     lcd.print("OFF    ");
   }
 
-  delay(50); // Update X milliseconds
+
+  // ----- Webpage backend logic -----
+//   if (client) {
+//     Serial.println("New Client.");
+//     String header = "";
+//     unsigned long currentTime = millis();
+//     unsigned long previousTime = currentTime;
+//     const long timeoutTime = 2000;
+
+//     while (client.connected() && currentTime - previousTime <= timeoutTime) {
+//       currentTime = millis();
+//       if (client.available()) {
+//         char c = client.read();
+//         header += c;
+//         if (c == '\n') {
+//           // ---- Always send headers ----
+//           client.println("HTTP/1.1 200 OK");
+//           client.println("Content-type:application/json");
+//           client.println("Access-Control-Allow-Origin: *");
+//           client.println("Connection: close");
+//           client.println();
+
+//           // ---- Routes ----
+//           if (header.indexOf("GET /data") >= 0) {
+//             client.print("{\"probe1\": {\"C\": ");
+//             client.print(tempsC[0]);
+//             client.print(", \"F\": ");
+//             client.print(tempsF[0]);
+//             client.print("}, \"probe2\": {\"C\": ");
+//             client.print(tempsC[1]);
+//             client.print(", \"F\": ");
+//             client.print(tempsF[1]);
+//             client.print("}}");
+//           }
+
+//           else if (header.indexOf("GET /18/on") >= 0) {
+//             output18State = "on";
+//             digitalWrite(output18, HIGH);
+//             client.print("{\"status\":\"GPIO 18 ON\"}");
+//           } else if (header.indexOf("GET /18/off") >= 0) {
+//             output18State = "off";
+//             digitalWrite(output18, LOW);
+//             client.print("{\"status\":\"GPIO 18 OFF\"}");
+//           }
+
+//           else if (header.indexOf("GET /19/on") >= 0) {
+//             output19State = "on";
+//             digitalWrite(output19, HIGH);
+//             client.print("{\"status\":\"GPIO 19 ON\"}");
+//           } else if (header.indexOf("GET /19/off") >= 0) {
+//             output19State = "off";
+//             digitalWrite(output19, LOW);
+//             client.print("{\"status\":\"GPIO 19 OFF\"}");
+//           }
+
+//           break;
+//         }
+//       }
+//     }
+//     client.stop();
+//     Serial.println("Client disconnected.");
+//   }
+
+  delay(50);
 }
